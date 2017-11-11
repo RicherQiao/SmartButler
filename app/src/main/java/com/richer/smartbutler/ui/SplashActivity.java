efb6d0ba1fd181488380a56b2a2fb65090b5df4e
RicherQiao <richerqiao@163.com>
2017-11-12 00:33:33 +0800

    Initial commit
    Signed-off-by: RicherQiao <richerqiao@163.com>

--- app/src/main/java/com/richer/smartbutler/ui/SplashActivity.java
+++ app/src/main/java/com/richer/smartbutler/ui/SplashActivity.java
@@ -44,7 +44,8 @@ public class SplashActivity extends AppCompatActivity{
                     if (isFirst()){
                         startActivity(new Intent(SplashActivity.this,GuideActivity.class));
                     }else{
-                        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                        startActivity(new Intent(SplashActivity.this,GuideActivity.class));
+                        //startActivity(new Intent(SplashActivity.this,MainActivity.class));
                     }
                     finish();
                     break;
